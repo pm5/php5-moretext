04434756b2e0d2f9b64b2c914696173893e906cb
Pomin Wu <pomin5@gmail.com>
2015-09-04 02:45:35 +0800

    add composer info

--- test/try.php
+++ test/try.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once __DIR__ . '/../src/moretext.php';
+require __DIR__ . "/vendor/autoload.php";
 
 $moretext = new Moretext\Client();
 foreach ($moretext->get(5, [20, 30]) as $sentence) {
